b86e653591a60905c6f1bbd6248a723abf32d6c6
littlecow <whu_jianliu@163.com>
2024-11-09 18:36:14 +0800

    electron vue3框架

---
+++ src/renderer/components/title-bar/title-bar.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="window-title" v-if="!IsUseSysTitle && isNotMac && !IsWeb">
+    <!-- 软件logo预留位置 -->
+    <div style="-webkit-app-region: drag" class="logo">
+      <img src="@renderer/assets/icons/favicon.ico" class="icon-logo" />
+    </div>
+    <!-- 菜单栏 -->
+    <div class="menu">
+      <div v-for="item in ['文件', '编辑', '选择', '运行']" class="menu_item">
+        {{ item }}
+      </div>
+    </div>
+    <!-- 中间标题位置 -->
+    <div style="-webkit-app-region: drag" class="title">littlecow</div>
+  </div>
+  <div v-else-if="!IsUseSysTitle && !isNotMac" class="window-title"></div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const { ipcRendererChannel, systemInfo } = window;
+
+const IsUseSysTitle = ref(false);
+const mix = ref(false);
+const isNotMac = ref(false);
+const IsWeb = ref(Boolean(__ISWEB__));
+
+isNotMac.value = systemInfo.platform !== "darwin";
+
+ipcRendererChannel.IsUseSysTitle.invoke().then((res) => {
+  IsUseSysTitle.value = res;
+});
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.window-title {
+  width: 100%;
+  height: 30px;
+  line-height: 30px;
+  background-color: #fff;
+  display: flex;
+  -webkit-app-region: drag;
+  position: fixed;
+  top: 0;
+  z-index: 99999;
+
+  .icon-logo {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+  }
+
+  .title {
+    width: 80%;
+    text-align: center;
+    color: #9d9d9d;
+  }
+
+  .menu {
+    height: 100%;
+    width: 20%;
+    display: flex;
+    // justify-content: space-around;
+
+    .menu_item {
+      margin-left: 2%;
+    }
+  }
+
+  .logo {
+    margin: 0 10px;
+  }
+
+  .controls-container {
+    display: flex;
+    flex-grow: 0;
+    flex-shrink: 0;
+    text-align: center;
+    position: relative;
+    z-index: 3000;
+    -webkit-app-region: no-drag;
+    height: 100%;
+    width: 138px;
+    margin-left: auto;
+
+    .windows-icon-bg {
+      display: inline-block;
+      -webkit-app-region: no-drag;
+      height: 100%;
+      width: 33.34%;
+      color: rgba(129, 129, 129, 0.6);
+
+      .icon-size {
+        width: 12px;
+        height: 15px;
+        vertical-align: -0.15em;
+        fill: currentColor;
+        overflow: hidden;
+      }
+    }
+
+    .windows-icon-bg:hover {
+      background-color: rgba(182, 182, 182, 0.2);
+      color: #333;
+    }
+
+    .close-icon:hover {
+      background-color: rgba(232, 17, 35, 0.9);
+      color: #fff;
+    }
+  }
+}
+</style>
---
+++ src/renderer/views/landing-page/LandingPage.vue
@@ -0,0 +1,340 @@
+<template>
+  <div id="wrapper">
+    <div class="footer">底部状态栏</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import SystemInformation from "./components/system-info-mation.vue";
+import { message } from "@renderer/api/login";
+import logo from "@renderer/assets/logo.png";
+import { onUnmounted, ref } from "vue";
+import { i18nt, setLanguage, globalLang } from "@renderer/i18n";
+import { useStoreTemplate } from "@store/template";
+
+const { ipcRendererChannel, shell, crash } = window;
+
+// if (!ipcRenderer) {
+//   ipcRenderer = {} as any;
+//   ipcRenderer.on =
+//     ipcRenderer.invoke =
+//     ipcRenderer.removeAllListeners =
+//     (...args: any): any => {
+//       console.log("not electron");
+//     };
+// }
+
+const percentage = ref(0);
+const colors = ref([
+  { color: "#f56c6c", percentage: 20 },
+  { color: "#e6a23c", percentage: 40 },
+  { color: "#6f7ad3", percentage: 60 },
+  { color: "#1989fa", percentage: 80 },
+  { color: "#5cb87a", percentage: 100 },
+] as string | ColorInfo[]);
+const dialogVisible = ref(false);
+const progressStaus = ref(null);
+const filePath = ref("");
+const updateStatus = ref("");
+const showForcedUpdate = ref(false);
+
+const storeTemplate = useStoreTemplate();
+
+console.log(`storeTemplate`, storeTemplate.getTest);
+console.log(`storeTemplate`, storeTemplate.getTest1);
+console.log(`storeTemplate`, storeTemplate.$state.testData);
+
+setTimeout(() => {
+  storeTemplate.TEST_ACTION("654321");
+  console.log(`storeTemplate`, storeTemplate.getTest1);
+}, 1000);
+
+const elPageSize = ref(100);
+const elCPage = ref(1);
+
+function changeLanguage() {
+  setLanguage(globalLang.value === "zh-cn" ? "en" : "zh-cn");
+}
+
+function startCrash() {
+  crash.start();
+}
+
+function openNewWin() {
+  const data = {
+    url: "/form/index",
+  };
+  ipcRendererChannel.OpenWin.invoke(data);
+  // ipcRenderer.invoke("open-win", data);
+}
+function getMessage() {
+  message().then((res) => {
+    // ElMessageBox.alert(res.data, "提示", {
+    //   confirmButtonText: "确定",
+    // });
+  });
+}
+function StopServer() {
+  ipcRendererChannel.StopServer.invoke().then((res) => {
+    // ElMessage({
+    //   type: "success",
+    //   message: "已关闭",
+    // });
+  });
+}
+function StartServer() {
+  ipcRendererChannel.StartServer.invoke().then((res) => {
+    if (res) {
+      // ElMessage({
+      //   type: "success",
+      //   message: res,
+      // });
+    }
+  });
+}
+// 获取electron方法
+function open() {}
+function CheckUpdate(data) {
+  switch (data) {
+    case "one":
+      ipcRendererChannel.CheckUpdate.invoke();
+      console.log("启动检查");
+      break;
+    case "two":
+      ipcRendererChannel.StartDownload.invoke("https://xxx").then(() => {
+        dialogVisible.value = true;
+      });
+      break;
+    case "three":
+      ipcRendererChannel.HotUpdate.invoke();
+      break;
+    case "four":
+      showForcedUpdate.value = true;
+      break;
+
+    default:
+      break;
+  }
+}
+function handleClose() {
+  dialogVisible.value = false;
+}
+
+ipcRendererChannel.DownloadProgress.on((event, arg) => {
+  percentage.value = Number(arg);
+});
+ipcRendererChannel.DownloadError.on((event, arg) => {
+  if (arg) {
+    progressStaus.value = "exception";
+    percentage.value = 40;
+    colors.value = "#d81e06";
+  }
+});
+ipcRendererChannel.DownloadPaused.on((event, arg) => {
+  if (arg) {
+    progressStaus.value = "warning";
+    // ElMessageBox.alert("下载由于未知原因被中断！", "提示", {
+    //   confirmButtonText: "重试",
+    //   callback: (action) => {
+    //     ipcRenderer.invoke("start-download");
+    //   },
+    // });
+  }
+});
+ipcRendererChannel.DownloadDone.on((event, age) => {
+  filePath.value = age.filePath;
+  progressStaus.value = "success";
+  // ElMessageBox.alert("更新下载完成！", "提示", {
+  //   confirmButtonText: "确定",
+  //   callback: (action) => {
+  //     shell.shell.openPath(filePath.value);
+  //   },
+  // });
+});
+// electron-updater upload
+ipcRendererChannel.UpdateMsg.on((event, age) => {
+  switch (age.state) {
+    case -1:
+      const msgdata = {
+        title: "发生错误",
+        message: age.msg as string,
+      };
+      dialogVisible.value = false;
+      ipcRendererChannel.OpenErrorbox.invoke(msgdata);
+      break;
+    case 0:
+      console.log("check-update");
+      break;
+    case 1:
+      dialogVisible.value = true;
+      console.log("has update download-ing");
+      break;
+    case 2:
+      console.log("not new version");
+      break;
+    case 3:
+      percentage.value = Number(
+        (age.msg as { percent: number }).percent.toFixed(1)
+      );
+      break;
+    case 4:
+      progressStaus.value = "success";
+      ipcRendererChannel.ConfirmUpdate.invoke();
+      break;
+    default:
+      break;
+  }
+});
+ipcRendererChannel.UpdateProcessStatus.on((event, msg) => {
+  switch (msg.status) {
+    case "downloading":
+      console.log("正在下载");
+      break;
+    case "moving":
+      console.log("正在移动文件");
+      break;
+    case "finished":
+      console.log("成功,请重启");
+      break;
+    case "failed":
+      console.log("msg.message.message");
+      break;
+
+    default:
+      break;
+  }
+  console.log(msg);
+  updateStatus.value = msg.status;
+});
+// onUnmounted(() => {
+//   console.log("销毁了哦");
+//   ipcRenderer.removeAllListeners("confirm-message");
+//   ipcRenderer.removeAllListeners("download-done");
+//   ipcRenderer.removeAllListeners("download-paused");
+//   ipcRenderer.removeAllListeners("confirm-stop");
+//   ipcRenderer.removeAllListeners("confirm-start");
+//   ipcRenderer.removeAllListeners("confirm-download");
+//   ipcRenderer.removeAllListeners("download-progress");
+//   ipcRenderer.removeAllListeners("download-error");
+// });
+</script>
+
+<style scoped lang="scss">
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: "Source Sans Pro", sans-serif;
+}
+
+#wrapper {
+  height: 100%;
+  width: 100%;
+  background: rgba(57, 57, 57, 1);
+  overflow: scroll;
+  position: relative;
+}
+
+::-webkit-scrollbar {
+  display: none;
+}
+
+.footer {
+  color: rgba(255, 255, 255, 0.8);
+  position: fixed;
+  margin-bottom: 0;
+  height: 3vh;
+  width: 100vw;
+  background-color: rgb(0, 0, 0);
+  top: 97vh;
+}
+
+#logo {
+  height: auto;
+  margin-bottom: 20px;
+  width: 420px;
+}
+
+main {
+  display: flex;
+  justify-content: space-between;
+}
+
+main > div {
+  flex-basis: 50%;
+}
+
+.left-side {
+  display: flex;
+  flex-direction: column;
+}
+
+.welcome {
+  color: #555;
+  font-size: 23px;
+  margin-bottom: 10px;
+}
+
+.title {
+  color: #2c3e50;
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 6px;
+}
+
+.title.alt {
+  font-size: 18px;
+  margin-bottom: 10px;
+}
+
+.doc {
+  margin-bottom: 10px;
+}
+
+.doc p {
+  color: black;
+  margin-bottom: 10px;
+}
+
+.doc {
+  button {
+    margin-top: 10px;
+    margin-right: 10px;
+  }
+
+  .btu {
+    display: inline-block;
+    line-height: 1;
+    white-space: nowrap;
+    cursor: pointer;
+    color: #fff;
+    background-color: #409eff;
+    border: 1px solid #409eff;
+    text-align: center;
+    box-sizing: border-box;
+    outline: none;
+    transition: 0.1s;
+    font-weight: 500;
+    padding: 12px 20px;
+    font-size: 14px;
+    border-radius: 4px;
+  }
+
+  .btu:focus,
+  .btu:hover {
+    background: #3a8ee6;
+    border-color: #3a8ee6;
+  }
+}
+
+.doc .button + .button {
+  margin-left: 0;
+}
+
+.conten {
+  text-align: center;
+}
+</style>
